fad193deafe1708e7a53289bb77272d8db227de8
Konrad Szydlo <konrad.szydlo@yahoo.co.uk>
2015-06-15 13:50:44 +0100

    example of reduce with internal exception raised

--- shop_rspec/spec/exception_spec.rb
+++ shop_rspec/spec/exception_spec.rb
@@ -16,6 +16,16 @@ class Run
     raise ArgumentError, "time must be positive" unless new_time > 0
     @time += new_time
   end
+
+  def add_runs runs
+    runs.reduce(0) do |sum, run|
+      raise ArgumentError, "run should be a number" unless run.is_a? Numeric
+      sum += run
+    end
+
+  rescue ArgumentError => e
+    "ERROR: #{e.message}"
+  end
 end
 
 
@@ -32,6 +42,19 @@ describe Run do
   describe "#add_more_time" do
     it "raises ArgumentError when time is negative" do
       expect { subject.add_more_time(-3)}.to raise_error(ArgumentError, "time must be positive")
+    end
+  end
+
+  describe "#add_runs" do
+    it "rescues when argument exception is raised" do
+      expect(subject.add_runs([5, 8, 'c', 2])).to eq("ERROR: run should be a number")
+    end
+  end
+
+  # not testing raising errors but how to use reduce method with an error within
+  describe "#add_runs" do
+    it "returns total distance run" do
+      expect(subject.add_runs([2, 4, 3, 1])).to eq(10)
     end
   end
 end
