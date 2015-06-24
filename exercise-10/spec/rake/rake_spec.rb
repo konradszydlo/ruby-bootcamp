bba2491c51cf1eac24c9ab113d2d6cdb9530bff3
Konrad Szydlo <konrad.szydlo@yahoo.co.uk>
2015-06-24 16:19:38 +0100

    running rake from rspec

--- exercise-10/spec/rake/rake_spec.rb
+++ exercise-10/spec/rake/rake_spec.rb
@@ -1,17 +1,9 @@
 require 'rake'
-# load 'Rakefile'
+load 'Rakefile'
 
 describe 'rake task' do
 
-  # before :all do
-  #   Rake.application = Rake::Application.new
-  #   Rake.application.init
-  #   Rake.application.load_rakefile
-  # end
-  #
-  # it 'should list all files' do
-  #   # Rake::Task['spec'].invoke
-  #   # Rake::Task['list'].invoke('resources/')
-  #   Rake.application['list'].invoke('resources/')
-  # end
+  it 'should list all files' do
+    Rake::Task['list'].invoke('resources/')
+  end
 end
