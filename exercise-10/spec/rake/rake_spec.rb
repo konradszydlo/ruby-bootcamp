f6945a2e5cc8972808a97237d2fd241507ecaf5f
Konrad Szydlo <konrad.szydlo@yahoo.co.uk>
2015-06-24 15:55:54 +0100

    list rake task that displays a list of files in a dir

---
+++ exercise-10/spec/rake/rake_spec.rb
@@ -0,0 +1,17 @@
+require 'rake'
+# load 'Rakefile'
+
+describe 'rake task' do
+
+  # before :all do
+  #   Rake.application = Rake::Application.new
+  #   Rake.application.init
+  #   Rake.application.load_rakefile
+  # end
+  #
+  # it 'should list all files' do
+  #   # Rake::Task['spec'].invoke
+  #   # Rake::Task['list'].invoke('resources/')
+  #   Rake.application['list'].invoke('resources/')
+  # end
+end
